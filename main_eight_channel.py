21d5411db9748e0032fffdff23f2ac8502b233c3
jinghuahe123 <electronicsexpert1@outlook.com>
2024-08-19 20:51:59 +0100

    commit

--- main_eight_channel.py
+++ main_eight_channel.py
@@ -1,16 +1,15 @@
 import sounddevice as sd, numpy as np, scipy.io.wavfile as wav
 
-# Function to record audio
+# record audio
 def record_audio(duration, sample_rate=48000, channels=8):
     print(f"Recording {channels}-channel audio for {duration} seconds...")
     recording = sd.rec(int(sample_rate * duration), samplerate=sample_rate, channels=channels, dtype='int16')
-    sd.wait()  # Wait until recording is finished
+    sd.wait() 
     return recording
 
-# Function to save the recording to a WAV file
+# save the recording as .wav
 def save_audio(filename, recording, sample_rate=48000):
-    # Normalize recording to the range [-1, 1]
-    recording = recording / np.max(np.abs(recording), axis=0)
+    recording = recording / np.max(np.abs(recording), axis=0) # Normalize recording to the range [-1, 1]
     wav.write(filename, sample_rate, recording)
     print(f"Recording saved as {filename}")
 
